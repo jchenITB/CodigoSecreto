f55eceababb7b8d3084ff953374635ab01aace7f
Jiafu Chen <94678822+jchenITB@users.noreply.github.com>
2022-04-25 12:12:09 +0200

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+const codigo = [];
+const maxIntento = 8;
+
+/*1. Genera una constante CODIGO_SECRETO de tipo array de 5 número aleatorios entre 0 y 9 usando la libreria Math.random();*/
+function codigoSecreto() {
+    for (let i = 0; i < 5; i++) {
+        codigo[i] = Math.floor((Math.random() * 10));
+    }
+}
+
+codigoSecreto();
+
+
+
