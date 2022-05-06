9901986fe8f7e8f352dfd75ac41ef3d9fc03b506
Jiafu Chen <jiafu.chen.7e5@itb.cat>
2022-05-06 12:52:34 +0200

    done

--- script.js
+++ script.js
@@ -1,28 +1,132 @@
-const codigo = [];
-var maxIntento = 8;
+const code = [];
+var maxTry = 8;
+var currentTry
+var size = 5
 
-/*1. Genera una constante CODIGO_SECRETO de tipo array de 5 número aleatorios entre 0 y 9 usando la libreria Math.random();*/
-function codigoSecreto() {
-    for (let i = 0; i < 5; i++) {
-        codigo[i] = Math.floor((Math.random() * 10));
-    }
-    console.log(codigo)
-}
-
-codigoSecreto();
 
 document.addEventListener('DOMContentLoaded', function () {
-    console.log(2)
-    let resultField = document.getElementById('Result')
-    appendResultRowTo(resultField)
+    setup()
 }, false);
+
+document.getElementById('numero').addEventListener('keypress', (event) =>{
+    if(event.key.replace(/[^0-9.]/g, '') == ''){
+        event.preventDefault()
+    }
+    if(event.key == 'Enter'){
+        Comprobar()
+    }
+})
+
+let setup = () =>{
+    currentTry = 0
+    codigoSecreto();
+    let resultField = $('Result')
+    appendResultRowTo(resultField)
+    let numberInput = $('numero')
+    numberInput.value = ""
+    numberInput.attributes['maxlength'].value = size
+    $('restart').classList.add('hide')
+    $('check').classList.remove('hide')
+    $('infoBox').classList.remove('win','lose')
+    $('infoBox').classList.add('hide')
+    $('numero').removeAttribute('disabled')
+    let codeCels = $('codigo').getElementsByClassName("cel")
+    for (let i = 0; i < size ; i++) {
+        codeCels[i].innerText = "?"
+    }
+}
+
+
+let Comprobar = () => {
+    let guess = document.getElementById('numero')
+    
+    if(guess.value.length != size){
+        showInfo('Por favor, escribe '+size+' digitos')
+        return false
+    }
+
+    let guessList = guess.value.split('').map(str => Number(str))
+    if(currentTry < maxTry){
+        for(let i = 0; i < guessList.length ; i++){
+            drawResult(guessList[i],i)
+        }
+        currentTry++
+
+    }
+    guess.value = ''
+    guess.focus()
+    checkGameOver(guessList)
+    
+}
+
+let end = (className, message) => {
+    $('infoBox').classList.add(className)
+    $('info').innerText = message
+    $('infoBox').classList.remove('hide')
+    $('restart').classList.remove('hide')
+    $('check').classList.add('hide')
+    $('numero').setAttribute("disabled", "")
+}
+
+
+let drawResult = (guessNumber, position) =>{
+    let resultField = document.getElementsByClassName('rowResult')[currentTry].getElementsByClassName('celResult')[position]
+    resultField.innerText = guessNumber
+    let resultClass = checkNumber(guessNumber, position)
+    if(resultClass != "") resultField.classList.add(resultClass)
+}
+
+let checkNumber = (guessNumber, position) =>{
+    if(code[position] == guessNumber){
+        return 'correct'
+    }else{
+        if(code.includes(guessNumber)){
+            return 'exists'
+        }
+    }
+    return ""
+}
+
+let checkGameOver = (guessList) =>{
+    if(currentTry < maxTry && JSON.stringify(guessList)==JSON.stringify(code)){
+        end('win','CONGRATULATION! YOU WON!')
+        let codeCels = $('codigo').getElementsByClassName("cel")
+        for (let i = 0; i < size ; i++) {
+            codeCels[i].innerText = guessList[i]
+        }
+    }else if(currentTry == maxTry){
+        end('lose','YOU LOST!')
+    }
+}
+
+
+let showInfo = (info) =>{
+    $('numero').focus()
+    let infoBox = $('infoBox')
+    let infoBody = $('info')
+    if(infoBox.classList.contains('hide')){
+        infoBody.innerText = info
+        infoBox.classList.remove('hide')
+        setTimeout(() => {
+            infoBox.classList.add('hide')
+        }, 2000);
+    }    
+}
+
+
+let codigoSecreto = () => {
+    for (let i = 0; i < size; i++) {
+        code[i] = Math.floor((Math.random() * 10));
+    }
+    log(code)
+}
 
 
 let appendResultRowTo  = (container) => {   
     container.innerHTML = null 
-    for (let i = 0; i < maxIntento ; i++) {
+    for (let i = 0; i < maxTry ; i++) {
         let row = createResultRow()
-        for(let j = 0; j < 5 ; j++){
+        for(let j = 0; j < size ; j++){
             row.append(createResultField())
         }
         container.append(row)
@@ -45,7 +149,19 @@ let createResultField = () => {
 }
 
 let setting = () =>{
-    maxIntento = prompt('Quants intents vols?')
-    let resultField = document.getElementById('Result')
-    appendResultRowTo(resultField)
+    let newMaxTry = prompt('Quants intents vols?')
+    if(newMaxTry != null){
+        maxTry = newMaxTry
+        let resultField = $('Result')
+        appendResultRowTo(resultField)
+        setup()
+    }
+}
+
+let log = (x) =>{
+    console.log(x)
+}
+
+let $ = (id) =>{
+    return document.getElementById(id)
 }
